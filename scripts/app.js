f241894fe8a0206a58b37d4940a85cc95bbf41fa
dagothar <dagothar@gmail.com>
2016-01-06 00:33:48 +0100

    app is now functional

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,50 @@
-define('app', ['forest'], function(forest) {
+define('app', ['jquery', 'forest'], function($, _forest) {
 	
 	function start() {
 		
-		var f = new forest.Forest(10, 5, 0.6);
-		f.print();
+		var updateView = function(f) {
+			f.render($("#forest"));
+			$("#trees").val(f.alivePercentage() + '%');
+		};
+		
+		var forest = new _forest.Forest(80, 25, $("#density").val());		
+		updateView(forest);
+		
+		var stepTimer;
+		
+		var makeStep = function() {
+			var change = forest.step();
+			updateView(forest);
+			
+			if (!change) clearInterval(stepTimer);
+		};
+		
+		$("#reset").click(function(ev) {
+			forest = new _forest.Forest(80, 25, $("#density").val());
+			updateView(forest);
+			ev.preventDefault();
+		});
+		
+		$("#fire").click(function(ev) {
+			for (var y = 0; y < forest.getHeight(); ++y) { forest.ignite(0, y); };
+			updateView(forest);
+			ev.preventDefault();
+		});
+		
+		$("#step").click(function(ev) {
+			makeStep();
+			ev.preventDefault();
+		});
+		
+		$("#start").click(function(ev) {
+			stepTimer = setInterval(makeStep, 100);
+			ev.preventDefault();
+		});
+		
+		$("#stop").click(function(ev) {
+			clearInterval(stepTimer);
+			ev.preventDefault();
+		});
 	}
 	
 	return {
--- scripts/forest.js
+++ scripts/forest.js
@@ -1,10 +1,16 @@
-define('forest', ['array2d'], function(a2d) {
+define('forest', ['jquery', 'array2d'], function($, a2d) {
 	
 	function Forest(width, height, density) {
 		var State = {
 			DEAD: 0,
 			ALIVE: 1,
 			BURNING: 2
+		};
+		
+		var Render = {
+			0: 'dead',
+			1: 'alive',
+			2: 'burning'
 		};
 		
 		var width = width;
@@ -27,13 +33,20 @@ define('forest', ['array2d'], function(a2d) {
 		};
 		
 		var checkNeighbours = function(c, x, y) {
-			for (var i = x-1; i <= x+1; ++i) {
+			/*for (var i = x-1; i <= x+1; ++i) {
 				for (var j = y-1; j <= y+1; ++j) {
 					if (i >= 0 && i < width && j >= 0 && j < height) {
 						if (c.get(i, j) == State.BURNING) return true;
 					}
 				}
-			}
+			}*/
+			
+			try {
+				if (c.get(x-1, y) == State.BURNING) return true;
+				if (c.get(x+1, y) == State.BURNING) return true;
+				if (c.get(x, y-1) == State.BURNING) return true;
+				if (c.get(x, y+1) == State.BURNING) return true;
+			} catch (err) {};
 			
 			return false;
 		};
@@ -41,22 +54,56 @@ define('forest', ['array2d'], function(a2d) {
 		this.step = function() {
 			console.log("* step *");
 			
+			var change = false;
 			var old_cells = cells.clone();
 			old_cells.forEach(function(v, x, y) {
 				if (v == State.BURNING) {
 					cells.set(x, y, State.DEAD);
+					change = true;
 				} else if (v == State.ALIVE) {
 					if (checkNeighbours(old_cells, x, y)) {
 						cells.set(x, y, State.BURNING);
+						change = true;
 					}
 				}
 			});
+			
+			return change;
 		};
 		
 		this.ignite = function(x, y) {
 			if (cells.get(x, y) == State.ALIVE) {
 				cells.set(x, y, State.BURNING);
 			}
+		};
+		
+		this.render = function(el) {
+			var table = $("<table/>");
+			
+			for (var y = 0; y < height; ++y) {
+				var row = $(table[0].insertRow(-1));
+				for (var x = 0; x < width; ++x) {
+					var cell = $("<td/>");
+					cell.html("&nbsp;");
+					cell.addClass(Render[cells.get(x, y)]);
+					row.append(cell);
+				}
+			}
+			
+			el.html("");
+			el.append(table);
+		};
+		
+		this.countAlive = function() {
+			var alive = 0;
+			cells.forEach(function(v, x, y) { if (v == State.ALIVE) ++ alive; });
+			return alive;
+		};
+		
+		this.alivePercentage = function() {
+			var alive = 0;
+			cells.forEach(function(v, x, y) { if (v == State.ALIVE) ++ alive; });
+			return Math.round(100 * alive / (width * height));
 		};
 		
 		if (typeof(density) === "undefined") {
